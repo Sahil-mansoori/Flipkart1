01509012b06cdb118e53670cfafe5ea9bd5b5771
Sahil-mansoori <sahil997195@gmail.com>
2023-10-08 12:01:13 +0530

    initial commit

---
+++ JS folder/main.js
@@ -0,0 +1,161 @@
+
+// <================ grocery nav slider js ==============>
+var swiper = new Swiper(".pk", {
+    spaceBetween:32,
+    loop:true,
+   //  speed:1500,
+   //  autoplay: {
+   //     delay: 500,
+   //     disableOnInteraction: false,
+   //   },
+
+     breakpoints: {
+       320: {
+         slidesPerView: 4,
+        
+       },
+       768: {
+         slidesPerView: 8,
+       
+       },
+       1024: {
+         slidesPerView: 10,
+       
+       },
+       1200: {
+         slidesPerView: 10,
+         
+       },
+     },
+ });
+
+
+// <================ Hero section Initialize Swiper js ==============>
+
+  var swiper = new Swiper(".mySwiper", {
+    // spaceBetween: 30,
+    // effect: "fade",
+    loop:true,
+    speed:1000,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+// <================  Electronics slider js ==============>
+var swiper = new Swiper(".eletronics", {
+  spaceBetween:32,
+  loop:true,
+  speed:1100,
+  // autoplay: {
+  //    delay: 500,
+  //    disableOnInteraction: false,
+  //  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+   breakpoints: {
+     320: {
+       slidesPerView: 2,
+      
+     },
+     768: {
+       slidesPerView: 3,
+     
+     },
+     1024: {
+       slidesPerView: 4,
+     
+     },
+     1200: {
+       slidesPerView: 5,
+       
+     },
+   },
+});
+
+// <================ Beauty toys and more slider js ==============>
+var swiper = new Swiper(".beauty", {
+  spaceBetween:32,
+  loop:true,
+  speed:1100,
+  // autoplay: {
+  //    delay: 500,
+  //    disableOnInteraction: false,
+  //  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+   breakpoints: {
+     320: {
+       slidesPerView: 2,
+      
+     },
+     768: {
+       slidesPerView: 3,
+     
+     },
+     1024: {
+       slidesPerView: 4,
+     
+     },
+     1200: {
+       slidesPerView: 5,
+       
+     },
+   },
+});
+
+
+// <================ festive section slider js ==============>
+var swiper = new Swiper(".fest", {
+  // spaceBetween:30,
+  loop:true,
+  speed:800,
+  // autoplay: {
+  //    delay: 500,
+  //    disableOnInteraction: false,
+  //  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+   breakpoints: {
+    //  320: {
+    //    slidesPerView: 2,
+      
+    //  },
+    //  768: {
+    //    slidesPerView: 3,
+     
+    //  },
+    //  1024: {
+    //    slidesPerView: 2,
+     
+    //  },
+     1200: {
+       slidesPerView: 1,
+       
+     },
+   },
+});
+
+
+   
+
+   
